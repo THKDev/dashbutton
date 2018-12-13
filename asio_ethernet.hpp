0a07d465954a67aeab820946fc994a93fa348243
Kordelle, Thomas <thomas.kordelle@dieboldnixdorf.com>
2018-12-13 15:03:55 +0100

    First checkin

---
+++ asio_ethernet.hpp
@@ -0,0 +1,95 @@
+#pragma once
+
+#ifdef ASIO_STANDALONE
+    #include <asio/asio.hpp>
+    namespace io = asio;
+#else
+    #include <boost/asio.hpp>
+    namespace io = boost::asio;
+#endif
+
+#include <net/if_arp.h>
+#include <iostream>
+
+namespace asio {
+namespace phy {
+
+class ethernet {
+    public:
+        typedef io::detail::array<unsigned char, 6> bytes_type;
+        
+    public:
+        ethernet(io::ip::tcp::endpoint epoint, const std::string& devname = "enp3s0")
+        : endpoint(epoint),
+          devicename(devname),
+          valid(false) {
+            if (endpoint.address().is_v4()) {
+                valid = macaddr_ipv4();
+            }
+            else {
+                std::cerr << "IPv6 is not supported." << std::endl;
+                valid = false;
+            }
+        }
+        
+        bytes_type macaddr() const {
+            return mac;
+        }
+        
+        std::string to_string() const {
+            std::string str(20, '\0');
+            if (isValid()) {
+                sprintf(&str[0], "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+            }
+            return str;
+        }
+        
+        bool isValid() const {
+            return valid;
+        }
+        
+    private:
+        bool macaddr_ipv4() {
+            struct auto_close {
+               ~auto_close() { if (fd != -1) close(fd); }
+               int fd;
+            } handle;
+            struct arpreq areq;
+            struct sockaddr_in *sin;
+            struct in_addr addr;
+            
+            memcpy(&addr.s_addr, endpoint.address().to_v4().to_bytes().data(), 4);
+            memset(&areq, 0, sizeof(areq));
+            
+            sin = (struct sockaddr_in *) &areq.arp_pa;
+            sin->sin_family = AF_INET;
+            sin->sin_addr = addr;
+            
+            sin = (struct sockaddr_in *) &areq.arp_ha;
+            sin->sin_family = ARPHRD_ETHER;
+        
+            std::copy_n(devicename.cbegin(), 15, areq.arp_dev);
+        
+            if ((handle.fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+                perror("socket");
+                return false;
+            }    
+            
+            if (ioctl(handle.fd, SIOCGARP, (caddr_t) &areq) == -1) {
+                perror("-- Error: unable to make ARP request, error");
+                return false;
+            }
+            
+            std::copy_n(areq.arp_ha.sa_data, mac.size(), mac.begin());
+            return true;
+        }
+        
+    private:
+        io::ip::tcp::endpoint endpoint;
+        bytes_type              mac;
+        bool                    valid;
+        std::string             devicename;
+};
+
+} // namespace phy
+} // namespace asio
---
+++ main.cc
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <boost/program_options.hpp>
+#include "asio_ethernet.hpp"
+
+
+#ifdef ASIO_STANDALONE
+    namespace io = asio;
+#else
+    namespace io = boost::asio;
+#endif
+    
+namespace po = boost::program_options; 
+
+static const std::string ARG_HELP("help");
+static const std::string ARG_PORT("port");
+static const std::string ARG_DEV("dev");
+
+/**
+ * @brief The server class
+ */
+class server
+{
+    public:
+        server(io::io_context& io_context, const short port, const std::string &device)
+        : acceptor(io_context, io::ip::tcp::endpoint(io::ip::tcp::v4(), port)),
+          networkdevice(device) {
+            do_accept();
+        }
+
+    private:
+        void do_accept() {
+            std::cout << "Accept incoming connection on port " << acceptor.local_endpoint().port() << std::endl;
+            std::cout << "Using device " << networkdevice << " to resolve MAC address." << std::endl;
+            acceptor.async_accept([this](std::error_code ec, io::ip::tcp::socket socket) {
+                if (!ec) {
+                    std::cout << "Remote IP : " << socket.remote_endpoint().address().to_string() << std::endl;
+                    asio::phy::ethernet eth(socket.remote_endpoint(), networkdevice);
+                    if (eth.isValid()) {
+                        std::cout << "Remote MAC: " << eth.to_string() << std::endl;
+                    }
+                    socket.close();
+                }
+                do_accept();
+            });
+        }
+        
+    private:
+        io::ip::tcp::acceptor  acceptor;
+        std::string            networkdevice;
+};
+
+int main(int argc, char* argv[])
+{
+    try {
+        po::options_description desc("Allowed options");
+        desc.add_options()
+            (ARG_HELP.c_str(), "produce help message")
+            (ARG_PORT.c_str(), po::value<int>()->default_value(443), "port to listen on")
+            (ARG_DEV.c_str(), po::value<std::string>()->default_value("enp3s0"), "name of the device to resolve mac address");
+        
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);    
+        
+        if (vm.count(ARG_HELP)) {
+            std::cout << desc << std::endl;
+            return 1;
+        }
+        
+        io::io_context io_context;
+        server srv(io_context, vm[ARG_PORT].as<int>(), vm[ARG_DEV].as<std::string>());
+        io_context.run();
+    }
+    catch (std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+    }
+
+    return 0;
+}
